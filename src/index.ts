367011acb6bc83e713576ef33523a00caad48bb8
QuantumCoded <mrbspony@gmail.com>
2021-08-22 08:22:31 -0500

    launch minecraft server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { spawn } from 'child_process';
 import { Server } from 'ws';
 import express from 'express';
+import * as path from 'path';
+
 const app = express()
     .use((req, res) => res.sendFile(req.path || 'index.html', { root: process.cwd() }))
     .listen(process.env.PORT || 3000);
@@ -8,21 +10,20 @@ const app = express()
 const wss = new Server({ server: app });
 
 wss.on('connection', (ws) => {
+    const minecraft = spawn('../jdk-16.0.2/bin/java', ['-jar', 'server.jar', '--nogui'], {
+        stdio: ['pipe', 'pipe', 'pipe'],
+        cwd: path.join(process.cwd(), 'minecraft'),
+    });
+
+    minecraft.stdout.on('data', d => ws.send(d));
+
 	ws.on('message', m => {
-		const a = m.toString().replace(/\n+$/, '').split(' ');
+		/* const a = m.toString().replace(/\n+$/, '').split(' ');
 		const p = spawn(a.shift() || '', a, { stdio: ['pipe', 'pipe', 'pipe'] });
 		p.on('error', console.error);
 		p.stdout.setEncoding('utf-8').on('data', s => ws.send(s.replace(/\n+$/, '')));
-		p.stderr.setEncoding('utf-8').on('data', s => ws.send(s.replace(/\n+$/, '')));
+		p.stderr.setEncoding('utf-8').on('data', s => ws.send(s.replace(/\n+$/, ''))); */
+
+        minecraft.stdin.write(m.toString() + '\n');
 	});
 });
-
-/*
-const minecraft = spawn('../jdk-16.0.2/bin/java', ['-jar', 'server.jar', '--nogui'], {
-    stdio: ['pipe', 'pipe', 'pipe'],
-    cwd: path.join(process.cwd(), 'minecraft'),
-});
-
-minecraft.stdout.pipe(process.stdout);
-*/
-
