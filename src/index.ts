edc736583cb3ccebfb501c6d4812187cd4b02dba
QuantumCoded <mrbspony@gmail.com>
2021-08-22 07:31:59 -0500

    stop running minecraft

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { createServer } from 'http';
 import { createReadStream } from 'fs';
 import * as path from 'path';
 
+/*
 const minecraft = spawn('../jdk-16.0.2/bin/java', ['-jar', 'server.jar', '--nogui'], {
     stdio: ['pipe', 'pipe', 'pipe'],
     cwd: path.join(process.cwd(), 'minecraft'),
 });
 
 minecraft.stdout.pipe(process.stdout);
+*/
 
 createServer((req, res) => {
     createReadStream('index.html').pipe(res);
